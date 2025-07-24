02f31a49e943686ec048ce7c18c21c0d8e2744f9
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-07-24 19:18:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -13,10 +13,28 @@ int rec(int i,int m,vector<int>&suff,vector<int>&p,vector<vector<int>>&dp){
 public:
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>>dp(n,vector<int>(n,0));
         vector<int>suff(n);
         suff[n-1] = piles[n-1];
         for(int i=n-2;i>=0;i--) suff[i]=suff[i+1] + piles[i]; 
-        return rec(0,1,suff,piles,dp);
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=1;j--)
+            {
+                if(i+2*j >= piles.size())
+                {
+                    dp[i][j]= suff[i];
+                    continue;
+                }
+                int res = 0;
+                for(int x=1;x<=2*j && i+x <n;x++)
+                {
+                    res = max(res,suff[i] - dp[i+x][max(j,x)]);
+                }
+                dp[i][j] = res;
+            }
+        }
+        return dp[0][1];
     }
 };
