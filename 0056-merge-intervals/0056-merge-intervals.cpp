b9ddfe48e9cada06178a3a8bc07a751f7d03a05c
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-07-31 12:02:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& i) {
-        sort(i.begin(),i.end());
-        int f = i[0][0];
-        int s = i[0][1];
-
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if(n == 1) return intervals;
+        sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
-
-        for(int j=1;j<i.size();j++)
+        int f = intervals[0][0];
+        int s = intervals[0][1];
+        for(int i=1;i<n;i++)
         {
-            int tf = i[j][0];
-            int ts = i[j][1];
-            if(tf > s)
+            int tf = intervals[i][0];
+            int ts = intervals[i][1];
+            if(tf <= s) 
             {
+                s = max(s,ts);
+            }
+            else{
                 ans.push_back({f,s});
                 f = tf;
                 s = ts;
             }
-            else
-            {
-                s = max(s,ts);
-            }
         }
-        ans.push_back({f,s});
+        if(intervals[n-1][0] <= s)
+        {
+            ans.push_back({f,intervals[n-1][1]});
+        }
+        else
+        {
+            ans.push_back({intervals[n-1][0],intervals[n-1][1]});
+        }
         return ans;
     }
 };
