b7f59bf57e179e63f5e91acaaeb0e9bae496df50
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-04 20:45:02 +0530

    Time: 398 ms (49.92%), Space: 211.5 MB (49.44%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,24 +1,44 @@
 class Solution {
-private: 
-    int rec(int i,int buy,int n,int cnt,vector<int>&p,vector<vector<vector<int>>>&dp)
+    int rec(int i,bool buy,int cnt,vector<int>p)
     {
-        if(i == n || cnt == 2) return 0;
-        if(dp[i][buy][cnt] !=-1) return dp[i][buy][cnt];
-        int pr = 0;
+        if(cnt == 2) return 0;
+        if(i >= p.size()) return 0;
+
+        int t = 0;
         if(buy)
         {
-            pr = max(-p[i] + rec(i+1,0,n,cnt,p,dp),rec(i+1,1,n,cnt,p,dp));
+            t = max(-p[i] + rec(i+1,!buy,cnt,p) ,rec(i+1,buy,cnt,p));
         }
         else
         {
-            pr = max(p[i] + rec(i+1,1,n,cnt+1,p,dp),rec(i+1,0,n,cnt,p,dp));
+            t = max(p[i] + rec(i+1,!buy,cnt+1,p) ,rec(i+1,buy,cnt,p));
         }
-        return dp[i][buy][cnt] = pr;
+        return t;
     }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return rec(0,1,prices.size(),0,prices,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        dp[0][0][0] = 0;
+        dp[0][1][0] = -prices[0];
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=1;j>=0;j--)
+            {
+                for(int k=0;k<2;k++)
+                {
+                    if(j)
+                    {
+                        dp[i][j][k] = max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                    }
+                    else
+                    {
+                        dp[i][j][k] = max(prices[i] + dp[i+1][1][k+1],dp[i+1][0][k]);
+                    }
+                }
+            }
+        }
+        // return rec(0,true,0,prices);
+        return dp[0][1][0];
     }
 };
