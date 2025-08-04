addab061c3361b9900914c211f16b483b8152360
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-04 18:23:59 +0530

    Time: 0 ms (100%), Space: 17.1 MB (70.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,43 +1,45 @@
 class Solution {
-int rec(int i,int buy,vector<int>&prices)
-{
-    if(i == prices.size()-1)
-    {
-        if(buy) return 0;
-        return prices[prices.size()-1];
-    }
-    int p = 0;
-    if(buy)
-    {
-        p = max(-prices[i] + rec(i+1,0,prices) , rec(i+1,1,prices));
-    }
-    else
-    {
-        p = max(prices[i] + rec(i+1,1,prices), rec(i+1,0,prices));
-    }
-    return p;
-}
+// int rec(int i,int buy,vector<int>&prices)
+// {
+//     if(i == prices.size()-1)
+//     {
+//         if(buy) return 0;
+//         return prices[prices.size()-1];
+//     }
+//     int p = 0;
+//     if(buy)
+//     {
+//         p = max(-prices[i] + rec(i+1,0,prices) , rec(i+1,1,prices));
+//     }
+//     else
+//     {
+//         p = max(prices[i] + rec(i+1,1,prices), rec(i+1,0,prices));
+//     }
+//     return p;
+// }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        // vector<vector<int>>dp(n,vector<int>(2,0));
+        int take = -prices[0];
+        int not_take = 0;
 
-        dp[0][1] = -prices[0];
-        dp[0][0] = 0;
+        // dp[0][1] = -prices[0];
+        // dp[0][0] = 0;
         for(int i=1;i<n;i++)
         {
             for(int j= 0;j<2;j++)
             {
                 if(j)
                 {
-                    dp[i][j] = max(-prices[i]+dp[i-1][0] , dp[i-1][1]);
+                    take = max(-prices[i]+not_take ,take);
                 }
                 else
                 {
-                    dp[i][j] = max(prices[i]+dp[i-1][1] ,dp[i-1][0]);
+                    not_take = max(prices[i]+take ,not_take);
                 }
             }
         }
-        return max(dp[n-1][1],dp[n-1][0]);
+        return max(take,not_take);
     }
 };
