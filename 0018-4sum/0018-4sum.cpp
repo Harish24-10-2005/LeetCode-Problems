2e4cc8843dffba3b79bcdaac69195958f0d41775
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-10 19:20:55 +0530

    Time: 378 ms (7.98%), Space: 17.5 MB (57.22%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
         int n = nums.size();
+        vector<vector<int>>res;
         sort(nums.begin(),nums.end());
-        for(int i =0;i<n-3;i++)
+        for(int i=0;i<n-3;i++)
         {
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            if(i> 0 && nums[i-1] == nums[i]) continue;
+            
             for(int j=i+1;j<n-2;j++)
             {
-                if(j>i+1 && nums[j] == nums[j-1]) continue;
-                int l=j+1;int r=n-1;
+                if(j > i+1 && nums[j-1] == nums[j]) continue;
+                cout<<i<<endl;
+                int l =j+1;
+                int r =n-1;
                 while(l<r)
                 {
-                    long long s = nums[i] + nums[j] + nums[l] + nums[r];
-                    if(s == target)
+                    long long summ = (long long) nums[i] + nums[j] + nums[l] + nums[r];
+                    if(summ == target)
                     {
-                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        res.push_back({nums[i], nums[j], nums[l], nums[r]});
                         l++;
                         r--;
-                        while( l<r && nums[l] == nums[l-1]) l++;
-                        while( l<r && nums[r] == nums[r+1]) r--;
+                        while(l<r && nums[l] == nums[l-1]) l++;
+                        while(l<r && nums[r] == nums[r+1]) r--; 
                     }
-                    else if(s > target) r--;
+                    else if(summ > target) r--;
                     else l++;
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
