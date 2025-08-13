09b49dcfb7d8552fdd35061f7f50e230cb4f85b0
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-13 15:09:25 +0530

    Time: 357 ms (5.31%), Space: 26.9 MB (44.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int>p(height.size());
-        p[0] = height[0];
-        for(int i=1;i<height.size();i++)
+        int n = height.size();
+        vector<int>suf(n);
+        suf[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--)
         {
-            p[i] = max(p[i-1] , height[i]);
+            suf[i] = max(height[i],suf[i+1]);
+            cout<<suf[i]<<" ";
         }
-        vector<int>s(height.size());
-        s[height.size()-1] = height[height.size()-1];
-        for(int i=height.size()-2;i>=0;i--)
-        {
-            s[i] = max(s[i+1] , height[i]);
-        }
+        cout<<endl;
+        int pre = height[0];
         int ans = 0;
-        for(int i =0;i<height.size();i++)
+        for(int i=0;i<n;i++)
         {
-            ans+=(min(p[i],s[i]) - height[i]);
+            pre = max(pre,height[i]);
+            ans+=(min(pre,suf[i]) - height[i]);
+            cout<<pre<<" ";
         }
         return ans;
     }
