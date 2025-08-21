d220631b09446dce7feec5ffa520a477554e58c7
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-21 22:52:55 +0530

    Time: 0 ms (100%), Space: 95.1 MB (79.51%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        if(n2<n1) return findMedianSortedArrays(nums2,nums1);
+        int n =nums1.size();
+        int m = nums2.size();
+        if(m < n) return findMedianSortedArrays(nums2,nums1);
 
-        int low = 0;
-        int high = n1;
-
-        int left = (n1 + n2 +1)/2;
-
-        int n = n1 + n2;
-
-        while(low<=high)
+        int k = (n+m+1)/2;
+        int l = 0;
+        int r = n;
+        while(l<=r)
         {
-            int m1 = (low + high)/2;
-            int m2 = left - m1;
+            int m1 = l+(r-l)/2;
+            int m2 = k - m1;
 
             int l1 = INT_MIN;
             int l2 = INT_MIN;
             int r1 = INT_MAX;
             int r2 = INT_MAX;
 
-            if(m1 < n1) r1 = nums1[m1];
-            if(m2 < n2) r2 = nums2[m2];
+            if(m1-1>= 0) l1 = nums1[m1-1];
+            if(m2-1>= 0) l2 = nums2[m2-1];
 
-            if(m1-1 >= 0) l1 = nums1[m1-1];
-            if(m2-1 >= 0) l2 = nums2[m2-1];
+            if(m1<n) r1 = nums1[m1];
+            if(m2<m) r2 = nums2[m2];
 
             if(l1 <= r2 && l2 <= r1)
             {
-                if(n%2 == 1) return (double)max(l1,l2);
-                return (double)(max(l1,l2)+min(r1,r2))/2.0;
-            }
+                if((n+m) % 2 == 0)
+                {
+                    return (double)(max(l1,l2)+min(r1,r2))/2;
+                }
+                return (double)max(l1,l2);
+            } 
             if(l1 > r2)
             {
-                high = m1 -1;
+                r = m1-1;
             }
             else
             {
-                low = m1 + 1;
+                l = m1+1;
             }
         }
-        return 0;
+        return -1;
     }
 };
