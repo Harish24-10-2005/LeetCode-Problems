97499baa7ae7af8a94ff27a18711282019068c9b
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-08-29 19:12:17 +0530

    Time: 1 ms (8.89%), Space: 8.5 MB (48.89%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -3,18 +3,39 @@ public:
     long long flowerGame(int n, int m) {
         int o1 = 0;
         int e1 = 0;
-        for(int i=1;i<=n;i++)
-        {
-            if(i%2 == 0) e1+=1;
-            else o1+=1;
-        }
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(i%2 == 0) e1+=1;
+        //     else o1+=1;
+        // }
         int o2 = 0;
         int e2 = 0;
-        for(int i=1;i<=m;i++)
+        // for(int i=1;i<=m;i++)
+        // {
+        //     if(i%2 == 0) e2+=1;
+        //     else o2+=1;
+        // }
+        if(n%2 == 0)
         {
-            if(i%2 == 0) e2+=1;
-            else o2+=1;
+            o1 = n/2;
+            e1 = n/2;
         }
-        return (long long) o1*e2 + (long long)o2*e1;
+        else
+        {
+            o1 = (n/2) + 1;
+            e1 = o1-1;
+        }
+        if(m%2 == 0)
+        {
+            o2 = m/2;
+            e2 = m/2;
+        }
+        else
+        {
+            o2= (m/2) +1;
+            e2 = o2-1;
+        }
+        long long a = (long long)o1*e2 + (long long) o2*e1 ;
+        return a<0?1:a;
     }
 };
