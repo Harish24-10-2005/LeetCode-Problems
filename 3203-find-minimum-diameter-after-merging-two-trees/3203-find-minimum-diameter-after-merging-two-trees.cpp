2fdd62c6190076f6838d890951b2451e2e822af1
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-09-11 19:23:41 +0530

    Time: 298 ms (75.48%), Space: 302.9 MB (79.46%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,21 +1,6 @@
 class Solution {
-private :
-    void findD(int node,int dis,int par,vector<vector<int>>&adj,pair<int,int>&res)
-    {
-        if(dis>res.first)
-        {
-            res.first = dis;
-            res.second = node;
-        }
-        for(int i:adj[node])
-        {
-            if(i!=par)
-            {
-                findD(i,dis+1,node,adj,res);
-            }
-        }
-    }
-    void buildG(vector<vector<int>>& edges,vector<vector<int>>&adj)
+private:
+    void createadj(vector<vector<int>>&edges,int n,vector<vector<int>>&adj)
     {
         for(int i=0;i<edges.size();i++)
         {
@@ -25,25 +10,74 @@ private :
             adj[v].push_back(u);
         }
     }
+    void dfs(int node,vector<vector<int>>&adj,int& last,int d,int& md,vector<int>&vis)
+    {
+        vis[node] = 1;
+        if(d > md)
+        {
+            md = d;
+            last = node;
+        }
+        for(int i:adj[node])
+        {
+            if(vis[i] == 0)
+            {
+                dfs(i,adj,last,d+1,md,vis);
+            }
+        }
+    }
+    void dfs2(int node,vector<vector<int>>&adj,int& m,int l,vector<int>&vis)
+    {
+        vis[node] = 1;
+        l+=1;
+        m = max(m,l);
+        // cout<<node<<" "<<m<<endl;
+        for(int i:adj[node])
+        {
+            if(vis[i] == 0)
+            {
+                dfs2(i,adj,m,l,vis);
+            }
+        }
+    }
 public:
     int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        vector<vector<int>>g1(edges1.size()+1);
-        vector<vector<int>>g2(edges2.size()+1);
-        buildG(edges1,g1);
-        buildG(edges2,g2);
+        int n = edges1.size()+1;
+        int m = edges2.size()+1;
 
-        pair<int,int>res1 = {INT_MIN,-1};
-        pair<int,int>res2 = {INT_MIN,-1};
-        findD(0,0,-1,g1,res1);
-        findD(res1.second,0,-1,g1,res2);
-        int d1 = res2.first;
+        vector<vector<int>>adj1(n);
+        vector<vector<int>>adj2(m);
+        createadj(edges1,n,adj1);
+        createadj(edges2,m,adj2);
+        int l1 = 0;
+        int l2 = 0;
 
-        res1 = {INT_MIN,-1};
-        res2 = {INT_MIN,-1};
-        findD(0,0,-1,g1,res1);
-        findD(res1.second,0,-1,g1,res2);
-        int d2 = res2.first;
+        vector<int>vis(n,0);
+        int md = -1;
+        dfs(0,adj1,l1,0,md,vis);
+        vis.assign(m,0);
+        md = -1;
+        dfs(0,adj2,l2,0,md,vis);
 
-        return max({d1, d2, (d1 + 1) / 2 + (d2 + 1) / 2 + 1});
+        // cout<<l1<<endl<<l2<<endl;
+        int m1 = 0;
+        int m2 = 0;
+        
+        vis.assign(n,0);
+        dfs2(l1,adj1,m1,0,vis);
+        // cout<<"................."<<endl;
+        vis.assign(m,0);
+        dfs2(l2,adj2,m2,0,vis);
+
+        // cout<<m1<<endl<<m2<<endl;
+        m1--;
+        m2--;
+        int res1 = m1/2;
+        int res2 = m2/2;
+
+        if(m1%2!=0) res1+=1;
+        if(m2%2!=0) res2+=1;
+
+        return max({m1,m2,res1 + res2 + 1});
     }
 };
