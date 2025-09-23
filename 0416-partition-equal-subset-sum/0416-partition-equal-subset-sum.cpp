312228ef3365d2cca306f205c7fa24e544e625d3
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-09-23 12:11:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,32 +1,23 @@
 class Solution {
+private:
+    bool rec(int i,int tar,vector<int>&nums)
+    {
+        if(tar == 0) return true;
+        if( i== nums.size()) return false;
+        bool t = false;
+        if(tar - nums[i] >=0)
+        {
+            t = rec(i+1,tar-nums[i],nums);
+        }
+        bool nt = rec(i+1,tar,nums);
+        return t || nt;
+    }
 public:
     bool canPartition(vector<int>& nums) {
-        int t = 0;
-        for(int a:nums) t+=a;
-        if (t % 2 != 0) return false;
-        int n = nums.size();
-        vector<vector<bool>>dp(n,vector<bool>((t/2)+1,false));
-        for(int j=0;j<n;j++)
-        {
-            dp[j][0] = true;
-        }
-        if(nums[0]<=t/2)
-        {
-            dp[0][nums[0]] = true;
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int tar=1;tar<=t/2;tar++)
-            {
-                bool ntake = dp[i-1][tar];
-                bool take = false;
-                if(nums[i]<=tar)
-                {
-                    take = dp[i-1][tar-nums[i]];
-                }
-                dp[i][tar] = take || ntake;
-            }
-        }
-        return dp[n-1][t/2];
-    }   
+        int summ = accumulate(nums.begin(),nums.end(),0);
+        int tar = summ/2;
+        if(summ%2 != 0) return false;
+        return rec(0,tar,nums);
+        
+    }
 };
