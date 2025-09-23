c8689f9290f0132e5b62e482433f2c0c702fbaab
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-09-23 11:34:39 +0530

    Time: 11 ms (30.09%), Space: 13.3 MB (30.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,26 +1,50 @@
 class Solution {
+    // int rec(int i,int j,string w1,string w2,vector<vector<int>>&dp)
+    // {
+    //     if(j == w2.size()) return w1.size() - i;
+    //     if(i ==  w1.size()) return w2.size() - j;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(w1[i] == w2[j]) 
+    //     {
+    //         return dp[i][j] = rec(i+1,j+1,w1,w2,dp);
+    //     }
+    //     int r = 1 + rec(i+1,j+1,w1,w2,dp);
+    //     int d = 1 + rec(i+1,j,w1,w2,dp);
+    //     int in = 1 + rec(i,j+1,w1,w2,dp);
+    //     return dp[i][j] = min({r,d,in});
+    // }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0] = i;
-        for(int j=0;j<=m;j++) dp[0][j] = j;
 
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=m;j++)
+            dp[i][m] = n-i;
+        }
+        for(int i=0;i<m;i++)
+        {
+            dp[n][i] = m-i;
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
             {
-                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1 + min(
-                    dp[i-1][j-1],min(
-                        dp[i-1][j],
-                        dp[i][j-1]
-                    )
-                );
+                if(word1[i] == word2[j]) 
+                {
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else
+                {
+                    int r = 1 + dp[i+1][j+1];
+                    int d = 1 + dp[i+1][j];
+                    int in = 1 + dp[i][j+1];
+                    dp[i][j] = min({r,d,in});
+                }
             }
         }
-        return dp[n][m];
+        return dp[0][0];
     }
 };
