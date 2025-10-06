f6eedeeb9343fdf647e15024a4f2228d398c85ec
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-10-06 12:04:01 +0530

    Time: 34 ms (77.71%), Space: 54.4 MB (72.2%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,26 +1,38 @@
 class Solution {
 private:
-    void rec(int idx,string s,vector<string>&temp,vector<vector<string>>&res)
+    bool ispal(string s)
     {
-        if(idx == s.size()) res.push_back(temp);
-        for(int i=idx;i<s.size();i++)
+        string t = s;
+        reverse(s.begin(),s.end());
+        return s == t;
+    }
+    void rec(int node,string &s,vector<vector<string>>&res,vector<string>&temp)
+    {
+        if(node == s.size())
         {
-            string t = s.substr(idx,i - idx +1); 
-            string v = t;
-            reverse(t.begin(),t.end());
-            if(t == v)
+            if(!temp.empty())
             {
-                temp.push_back(v);
-                rec(i+1,s,temp,res);
+                res.push_back(temp);
+            }
+            return;
+        }
+        string t = "";
+        for(int i=node;i<s.size();i++)
+        {
+            t+=s[i]; 
+            if(ispal(t))
+            {
+                temp.push_back(t);
+                rec(i+1,s,res,temp);
                 temp.pop_back();
             }
         }
-    }   
+    }
 public:
     vector<vector<string>> partition(string s) {
-        vector<string>temp;
         vector<vector<string>>res;
-        rec(0,s,temp,res);
+        vector<string>temp;
+        rec(0,s,res,temp);
         return res;
     }
 };
