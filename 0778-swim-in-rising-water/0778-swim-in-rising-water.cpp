f6cfc8788f8f857f3b7bc2d44699c2d9ae9bbd2b
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-10-06 07:06:05 +0530

    Time: 159 ms (5.05%), Space: 12.9 MB (77.3%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -2,40 +2,46 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        int res =0;
-        priority_queue<pair<int,pair<int,int>>,
-                    vector<pair<int,pair<int,int>>>,
-                    greater<pair<int,pair<int,int>>>>pq;
         vector<vector<int>>vis(n,vector<int>(n,0));
+        int ans = 0;
+        vector<int>dx{0,-1,1,0};
+        vector<int>dy{1,0,0,-1};
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+
+        pq.push({grid[0][0],{0,0}});
         vis[0][0] = 1;
-        pq.push({grid[0][0], {0, 0}});
-
-        vector<int>dx{1,0,-1,0};
-        vector<int>dy{0,1,0,-1};
-
         while(!pq.empty())
         {
             auto a = pq.top();
             pq.pop();
-
+            int h = a.first;
             int x = a.second.first;
             int y = a.second.second;
-            int ht = a.first;
-            if(x == n-1 && y == n-1){
-                 res = ht;
-                 break;
-            }
-            for(int i=0;i<4;i++)
+            cout<<"x "<<x<<" y "<<y<<" h "<<h<<endl;
+            if(x == n-1 && y == n-1)
             {
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if(nx >=0 && ny >=0 && nx<n && ny<n && vis[nx][ny] !=1)
+                ans = h;
+                break;   
+            }
+            for(int d = 0;d<4;d++)
+            {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if(nx >= 0 && ny >=0 && nx < n && ny < n && vis[nx][ny] == 0)
                 {
                     vis[nx][ny] = 1;
-                    pq.push({max(ht, grid[nx][ny]), {nx, ny}});
+                    if(h >= grid[nx][ny])
+                    {
+                        pq.push({h,{nx,ny}});
+                    }
+                    else
+                    {
+                        pq.push({grid[nx][ny],{nx,ny}});
+                    }
                 }
             }
         }
-        return res;
+        return ans;
+
     }
 };
