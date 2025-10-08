21696724efeeb354b238fa0cc43896396b78f658
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-10-08 21:10:33 +0530

    Time: 19 ms (96.25%), Space: 26.9 MB (19.97%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -4,18 +4,26 @@ public:
         int n = str1.size();
         int m = str2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        dp[0][0] = 0;
+
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(str1[i-1] == str2[j-1]) dp[i][j] = dp[i-1][j-1] +1 ;
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                if(str1[i-1] == str2[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        string res = "";
         int i = n;
         int j = m;
-        while (i > 0 && j > 0)
+        string res = "";
+        while(i>0 && j>0)
         {
             if(str1[i-1] == str2[j-1])
             {
@@ -25,21 +33,22 @@ public:
             }
             else
             {
-                if(dp[i][j-1]>dp[i-1][j])
-                {
-                    res+=str2[j-1];
-                    j--;
-                }
-                else
+                if(dp[i-1][j] > dp[i][j-1])
                 {
                     res+=str1[i-1];
                     i--;
                 }
+                else
+                {
+                    res+=str2[j-1];
+                    j--;
+                }
             }
         }
-        while (i > 0) res += str1[i-1], i--;
-        while (j > 0) res += str2[j-1], j--;
-        reverse(res.begin(),res.end());
+        while (i > 0) res += str1[--i];
+        while (j > 0) res += str2[--j];
+
+        reverse(res.begin(), res.end());
         return res;
     }
 };
