861590cab61d7b4b173aa769e009637aa427b8fb
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-10-12 00:11:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (9.02%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int x = 0;
+        int y = 0;
+        int i = 0;
+        vector<vector<int>>d{{0,1},{1,0},{0,-1},{-1,0}};
+        for(auto a:instructions)
+        {
+            if(a == 'R')
+            {
+                i = (i+1)%4;
+            }
+            else if(a == 'L')
+            {
+                i = (i+3)%4;
+            }
+            else
+            {
+                x += d[i][0];
+                y += d[i][1];
+            }
+        }
+        return (x == 0 && y == 0) || i > 0;
+    }
+};
