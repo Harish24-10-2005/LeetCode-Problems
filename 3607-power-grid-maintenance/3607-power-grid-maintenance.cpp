ae1f7d2bfa08e91b899d0c4124380fd9da439ff1
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-11-06 19:23:37 +0530

    Time: 375 ms (53.82%), Space: 390.9 MB (49.6%) - LeetHub

--- 3607-power-grid-maintenance/3607-power-grid-maintenance.cpp
+++ 3607-power-grid-maintenance/3607-power-grid-maintenance.cpp
@@ -1,58 +1,85 @@
+class Disjoint{
+public:
+    vector<int>size;
+    vector<int>par;
+    Disjoint(int n){
+        size.resize(n);
+        par.resize(n);
+        for(int i=0;i<n;i++)
+        {
+            size[i] = 1;
+            par[i] = i;
+        }
+    }
+    int findpar(int node){
+        if(node == par[node]) return node;
+        return par[node] = findpar(par[node]);
+    }
+    void unionBysize(int u,int v){
+        int up = findpar(u);
+        int vp = findpar(v);
+        if(size[up]<size[vp])
+        {
+            par[up] = par[vp];
+            size[vp]+=size[up];
+        }
+        else{
+            par[vp] = par[up];
+            size[up]+=size[vp];
+        }
+    }
+};
+
 class Solution {
 public:
-    
-    vector<int> par;
-    
-    int find(int x) {
-        if (x != par[x])
-            par[x] = find(par[x]);
-        
-        return par[x];
-    }
-    
-    void unite(int x, int y) {
-        x = find(x);
-        y = find(y);
-        
-        if (x != y) par[y] = x;
-    }
-
     vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
-        par.resize(c + 1);
-        
-        for (int i = 1; i <= c; i++) par[i] = i;
-
-        for (auto& c : connections) {
-            unite(c[0], c[1]);
-        }
-
-        unordered_map<int, set<int>> onN;
-        
-        for (int i = 1; i <= c; i++) {
-            int comp = find(i);
-            onN[comp].insert(i);
-        }
-
-        vector<int> res;
-
-        for (auto& q : queries) {
-            int ty = q[0];
-            int x = q[1];
-            int comp = find(x);
-
-            if (ty == 1) {
-                if (onN[comp].count(x)) {
-                    res.push_back(x);
-                } else if (!onN[comp].empty()) {
-                    res.push_back(*onN[comp].begin());
-                } else {
-                    res.push_back(-1);
-                }
-            } else {
-                onN[comp].erase(x);
+        Disjoint d(c+1);
+        for(auto a:connections)
+        {
+            int u = a[0];
+            int v = a[1];
+            int up = d.findpar(u);
+            int vp = d.findpar(v);
+            if(up != vp)
+            {
+                d.unionBysize(up,vp);
             }
         }
+        unordered_map<int,set<int>>mpp;
 
+        for(int i=1;i<=c;i++)
+        {
+            int p = d.findpar(i);
+            mpp[p].insert(i);
+        }
+        vector<int>vis(c+1,1);
+        vector<int>res;
+        for(auto a:queries)
+        {
+            int c = a[0];
+            int node = a[1];
+            int p = d.findpar(node);
+            if(c == 1){
+                if(vis[node] == 1) res.push_back(node);
+                else{
+                    bool f = false;
+                    for(auto& a:mpp[p])
+                    {
+                        if(vis[a] == 1)
+                        {
+                            f = true;
+                            res.push_back(a);
+                            break;
+                        }
+                    }
+                    if(!f) res.push_back(-1);
+                }
+            }
+            else{
+                vis[node] = 0;
+                mpp[p].erase(node);
+            }
+        }
         return res;
     }
 };
