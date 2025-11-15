05598c991205279b0c0df3ae0064874c641e984e
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-11-15 19:18:26 +0530

    Time: 238 ms (5.03%), Space: 9.8 MB (93.08%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -0,0 +1,53 @@
+class Solution {
+private:
+    bool eval(string& e,int st, int end)
+    {
+        if (st > end) return false;
+        if(st == end)
+        {
+            return e[st] == 't';
+        }
+        for(int i=st;i<=end;i++)
+        {
+            cout<<e[i];
+        }
+        cout<<endl;
+        // cout<<st<<" "<<end;
+        char ch = e[st];
+        bool res = ch == '&' ? true: false;
+        int d = 0;
+        int l = st + 2;
+        if(ch == '!')
+        {
+            return !eval(e,l,end-1);
+        }
+        for(int i=st+2;i<end;i++)
+        {
+            if(e[i] == '(') d++;
+            if(e[i] == ')') d--;
+            if(e[i] == ',' && d == 0)
+            {
+                bool val = eval(e,l,i-1);
+                if(ch == '&')
+                {
+                    if(val == false) return false;
+                    res = res && val;
+                }
+                else
+                {
+                    if( val == true) return true;
+                    res = res || val;
+                }
+                l = i+1;
+            }
+        }
+        bool val = eval(e,l,end-1);
+        if(ch == '&') res = res && val;
+        else res = res || val;
+        return res;
+    }
+public:
+    bool parseBoolExpr(string e) {
+       return eval(e,0,e.size()-1);
+    }
+};
