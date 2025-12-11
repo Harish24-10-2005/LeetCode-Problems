4974b6d7e018e25f9fde0df1d23612fb09e406bc
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-12-11 21:27:24 +0530

    Time: 191 ms (28.71%), Space: 169.7 MB (25.49%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,43 +1,37 @@
 class Solution {
 private:
     vector<vector<int>>adj;
-    vector<int>res;
-    vector<int>vis;
-    int n;
-    int maxx = INT_MIN;
-    void dfs(int node,int temp)
-    {
-        res[node] = temp;
+    vector<int>vis,path,dep;
+    int maxx = -1;
+    void dfs(int node,int dist){
+        path[node] = 1;
         vis[node] = 1;
-        for(auto a:adj[node])
-        {
-            if(vis[a] == 0)
-            {
-                dfs(a,temp + 1);
+        dep[node] = dist;
+        for(int i:adj[node]){
+            if(!vis[i]){
+                dfs(i,dist+1);
             }
-            else if(vis[a] == 1)
-            {
-                maxx = max(maxx,temp - res[a]+ 1);
+            else if(path[i]){
+                maxx = max(maxx,dist - dep[i]  + 1);
             }
         }
-        vis[node] = 2;
+        path[node] = 0;
     }
-
 public:
     int longestCycle(vector<int>& edges) {
-        n = edges.size();
+        int n = edges.size();
         adj.resize(n);
-        res.assign(n, 0);
-        vis.assign(n, 0);
-
-        for(int i=0;i<n;i++)
-        {
+        vis.assign(n,0);
+        path.assign(n,0);
+        dep.assign(n,0);
+        for(int i = 0;i<n;i++){
             if(edges[i] != -1) adj[i].push_back(edges[i]);
         }
-        for(int i=0;i<n;i++)
-        {
-            if(vis[i] == 0) dfs(i,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,0);
+            }
         }
-        return maxx == INT_MIN ? -1 : maxx;
+        return maxx;
     }
 };
