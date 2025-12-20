abd29d96823d032b8ab48a17f97b9d9fd19eb8c5
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2025-12-20 08:39:34 +0530

    Time: 140 ms (74.19%), Space: 217.8 MB (33.7%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,59 +1,40 @@
-// class D{
-//     vector<int>size;
-//     vector<int>par;
-//     D(int n){
-//         par.resize(n);
-//         size.assign(n,1);
-//         for(int i=0;i<n;i++){
-//             par[i] = i;
-//         }
-//         int findpar(int n){
-//             if(n == par[n]) return n;
-//             return par[n] = findpar(par[n]);
-//         }
-//         int unionsize(int u,int v){
-//             int uu = par[u];
-//             int vu = par[v];
-//             if(s[uu] < s[vu]){
-//                 par[uu] = vu;
-//                 s[uv] += s[uu];
-//             }
-//             else{
-//                 par[vu] = uu;
-//                 s[uu] += s[vu];
-//             }
-//         }
-//     }
-// };
 class Solution {
 public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        map<int,unordered_set<int>>mpp;
-        // res.assign(n,0);
-        vector<int>res;
-        unordered_set<int>kn;
-        kn.insert(0);
-        kn.insert(firstPerson);
-        for(auto a:meetings){
-            mpp[a[2]].insert(a[0]);
-            mpp[a[2]].insert(a[1]);
+    using int2=pair<int, int>;
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson)
+    {
+        vector<vector<int2>> adj(n);
+        for(auto& meet: meetings){
+            int x=meet[0], y=meet[1], time=meet[2];
+            adj[x].emplace_back(time, y);
+            adj[y].emplace_back(time, x);
         }
-        for(auto a:mpp){
-            // if(a.first < time) continue;
-            bool f = false;
-            for(auto c:a.second){
-                if(f) break;
-                if(kn.count(c)){
-                    for(auto vec:a.second){
-                        kn.insert(vec);
-                    }
-                    f = true;
-                }
+        vector<int> known(n, INT_MAX);
+        vector<int> list;
+        priority_queue<int2, vector<int2>, greater<int2>> pq;
+        pq.emplace(0, 0);
+        pq.emplace(0, firstPerson);
+        while(!pq.empty()){
+            auto [s, x]=pq.top();
+            pq.pop();
+            if (known[x]!=INT_MAX) continue;
+            list.push_back(x);
+            known[x]=s;
+            for(auto& [t, y]: adj[x]){
+                if (known[y]!=INT_MAX ||t<s) continue;
+                pq.emplace(t, y);
+            
             }
         }
-        for(int i:kn){
-            res.push_back(i);
-        }
-        return res;
+        
+        return list;
     }
 };
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
