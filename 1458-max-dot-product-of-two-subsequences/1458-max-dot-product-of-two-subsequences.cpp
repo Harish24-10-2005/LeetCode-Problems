41583c7c085612bc14ca7e049bee2741b235cf43
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2026-01-08 10:57:48 +0530

    Time: 175 ms (5.2%), Space: 62.2 MB (6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,22 +1,25 @@
 class Solution {
 private:
     int n,m;
+    vector<vector<vector<int>>>dp;
     int rec(int i,int j,int si,vector<int>& nums1,vector<int>&nums2){
         if(i == n || j == m){
             if(si == 0) return INT_MIN;
             return 0;
         }
+        if(dp[i][j][si] != -1) return dp[i][j][si];
         int take = INT_MIN;
-        int temp = rec(i+1,j+1,si+1,nums1,nums2);
+        int temp = rec(i+1,j+1,1,nums1,nums2);
         if(temp != INT_MIN) take = nums1[i] * nums2[j] + temp;
         // int take = nums1[i] * nums2[j] + rec(i+1,j+1,si+1,nums1,nums2);
         int nottake = max(rec(i,j+1,si,nums1,nums2) , rec(i+1,j,si,nums1,nums2));
-        return max(take,nottake);
+        return dp[i][j][si] = max(take,nottake);
     }
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         n = nums1.size();
         m = nums2.size();
+        dp.assign(n,vector<vector<int>>(m,vector<int>(2,-1)));
         return rec(0,0,0,nums1,nums2);
     }
 };
