2f5567e43e159b4120249b8513ef43903a4911cb
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2026-01-27 14:23:09 +0530

    Time: 224 ms (78.85%), Space: 292 MB (65.86%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -1,23 +1,26 @@
 class Solution {
 private:
     vector<vector<pair<int,int>>>adj;
-    void dijis(int node,vector<int>&dist){
-        dist[node] = 0;
+    int dijis(int node,int n,vector<int>&dist){
+        // dist[node] = 0;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
         pq.push({0,node});
         while(!pq.empty()){
             int u = pq.top().second;
             int w = pq.top().first;
             pq.pop();
+            if(dist[u] != -1) continue;
+            dist[u] = w;
+            if(u == n-1) return w;
             for(auto e:adj[u]){
                 int v = e.first;
                 int c = e.second;
-                if(w + c < dist[v]){
-                    dist[v] = w + c;
+                if(dist[v] == -1){
                     pq.push({c + w,v});
                 }
             }
         }
+        return dist[n-1];
     }
 public:
     int minCost(int n, vector<vector<int>>& edges) {
@@ -27,30 +30,9 @@ public:
             int v = e[1];
             int w = e[2];
             adj[u].push_back({v,w});
+            adj[v].push_back({u,2 * w});
         }
-        vector<int>dist1(n,INT_MAX),dist2(n,INT_MAX);
-        dijis(0,dist1);
-        adj.clear();
-        adj.resize(n);
-        for(auto &e:edges){
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
-            adj[v].push_back({u,w});
-        }
-        dijis(n-1,dist2);
-        for(auto i:dist1) cout<<i<<" ";
-        cout<<endl;
-        for(auto i:dist2) cout<<i<<" ";
-
-        int ans = dist1[n-1];
-        for(auto &e:edges){
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
-            if(dist1[v] == INT_MAX || dist2[u] == INT_MAX) continue;
-            ans = min(ans,dist1[v] + dist2[u] + 2 * w);
-        }
-        return ans;
+        vector<int>dist1(n,-1);
+        return dijis(0,n,dist1);
     }
 };
