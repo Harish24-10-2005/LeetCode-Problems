502efe1d7bd46e8346a3054949d35366ed128815
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2026-02-03 17:21:14 +0530

    Time: 142 ms (24.5%), Space: 71 MB (21.37%) - LeetHub

--- 2258-escape-the-spreading-fire/2258-escape-the-spreading-fire.cpp
+++ 2258-escape-the-spreading-fire/2258-escape-the-spreading-fire.cpp
@@ -1,45 +1,46 @@
 class Solution {
     vector<int>dx{1,-1,0,0};
     vector<int>dy{0,0,-1,1};
-    vector<vector<int>>vis;
     int n,m;
 private:
-    int dfs(int i,int j,int minn,int idx,int numidx,vector<vector<int>>&grid){
-        if(i == n-1 && j == m-1){
-            return (minn - numidx);
-        }
-        if(i >= n || j >= m) return INT_MIN;
-
-        if(grid[i][j] <= idx) return INT_MIN;
-        int ans = INT_MIN;
-        for(int d = 0;d<4;d++){
-            int nx = i + dx[d];
-            int ny = j + dy[d];
-            if(nx < n && nx >= 0 && ny < m && ny >=0 && grid[nx][ny] != -1 && vis[nx][ny] == 0){
-                vis[nx][ny] = 1;
-                int temp = minn;
-                int temp_idx = numidx;
-                if(temp > grid[nx][ny]){
-                    temp = grid[nx][ny];
-                    temp_idx = idx + 1;
+    int bfs(int k,vector<vector<int>>&grid){
+        if(k >= grid[0][0]) return false;
+        vector<vector<int>>vis;
+        vis.assign(n,vector<int>(m,-1));
+        vis[0][0] = k;
+        queue<vector<int>>q;
+        q.push({0,0});
+        while(!q.empty()){
+            int x = q.front()[0];
+            int y = q.front()[1];
+            int t= vis[x][y];
+            q.pop();
+            for(int d = 0;d<4;d++){
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if(nx < n && nx >= 0 && ny < m && ny >=0 && grid[nx][ny] != -1 && vis[nx][ny] == -1){
+                    if(nx == n-1 && ny == m-1){
+                        if(t+1 <= grid[nx][ny]) return true;
+                    }
+                    else if(t+1 < grid[nx][ny]){
+                        vis[nx][ny] = t + 1;
+                        q.push({nx,ny});
+                    }
                 }
-                ans = max(ans,dfs(nx,ny,temp,idx + 1,temp_idx,grid));
-                vis[nx][ny] = 0;
             }
         }
-        return ans;
+        return false;
     }
 public:
     int maximumMinutes(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
-        vis.assign(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2) grid[i][j] = -1;
                 else if(grid[i][j] == 1){
-                    pq.push({1,{i,j}});
+                    pq.push({0,{i,j}});
                 }
                 else{
                     grid[i][j] = INT_MAX;
@@ -61,12 +62,19 @@ public:
                 }
             }
         }
-        vis[0][0] = 1;
-        int ans = dfs(0,0,grid[0][0],1,1,grid);
-        cout<<ans<<endl;
-        // cout<<INT_MAX;
-        if(ans == 0) return 0;
-        if(ans == INT_MIN) return -1;
-        return ans == INT_MAX-1 ? 1e9 : ans-1;
+        int l = 0;
+        int r = 1e9;
+        int ans = -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(bfs(mid,grid)){
+                ans = mid;
+                l = mid +1;
+            }
+            else{
+                r = mid -1;
+            }
+        }
+        return ans;
     }
 };
