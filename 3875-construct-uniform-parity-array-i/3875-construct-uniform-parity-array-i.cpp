36defdb1f3553f3cdea0b499ae0b2628574c89f7
HARISH R <143640985+Harish24-10-2005@users.noreply.github.com>
2026-03-23 18:01:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        int minn = *min_element(nums1.begin(),nums1.end());
-        int o = INT_MAX;
-        int e = INT_MAX;
-        for(int i:nums1){
-            if(i%2 == 0) e = min(e,i);
-            else o = min(o,i);
-        }
-        bool f = (minn % 2 == 0);
-        for(int i:nums1){
-            if(f){
-                if(i%2 != 0){
-                    // if((i - o) %2 != 0) return false;
-                }
-            }else{
-                if(i%2 == 0){
-                    // if((i - o) %2 == 0) return false;
-                }
-            }
-        }
+        // int n = nums1.size();
+        // int minn = *min_element(nums1.begin(),nums1.end());
+        // int o = INT_MAX;
+        // int e = INT_MAX;
+        // for(int i:nums1){
+        //     if(i%2 == 0) e = min(e,i);
+        //     else o = min(o,i);
+        // }
+        // bool f = (minn % 2 == 0);
+        // for(int i:nums1){
+        //     if(f){
+        //         if(i%2 != 0){
+        //             // if((i - o) %2 != 0) return false;
+        //         }
+        //     }else{
+        //         if(i%2 == 0){
+        //             // if((i - o) %2 == 0) return false;
+        //         }
+        //     }
+        // }
         return true;
     }
 };
